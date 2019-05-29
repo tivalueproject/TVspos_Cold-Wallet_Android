0763a4841936b6e6534b6c373e7aa367d495e5b4
ocean <3408598@qq.com>
2019-05-29 16:45:48 +0800

    modify download

--- app/src/main/java/systems/v/coldwallet/ui/view/SplashActivity.java
+++ app/src/main/java/systems/v/coldwallet/ui/view/SplashActivity.java
@@ -69,7 +69,6 @@ public class SplashActivity extends BaseActivity {
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
                                                 DownloadApp(appUrl,String.valueOf(serverVersion));
-                                                launch();
                                             }
                                         })
                                         .setNegativeButton(R.string.basic_alert_dialog_cancel, new DialogInterface.OnClickListener() {
@@ -189,23 +188,19 @@ public class SplashActivity extends BaseActivity {
         new systems.v.wallet.utils.DownloadUtil(new systems.v.wallet.utils.DownloadUtil.DownloadCallBack() {
             @Override
             public void downLoadStart(systems.v.wallet.utils.DownloadUtil.DownloadParam param) {
-//                pb_update.setVisibility(View.VISIBLE);
-//                tv_update.setVisibility(View.GONE);
+                showLoading();
             }
 
             @Override
             public void onProgressUpdate(systems.v.wallet.utils.DownloadUtil.DownloadParam param, int downloadSize, int totalSize) {
-//                pb_update.setMax(totalSize);
-//                pb_update.setProgress(downloadSize);
             }
 
             @Override
             public void downloadEnd(systems.v.wallet.utils.DownloadUtil.DownloadParam param) {
+                hideLoading();
                 if (param.getSaveFile().exists()) {
                     systems.v.wallet.utils.IntentUtils.installApk(mActivity, param.getSaveFile());
                 }
-//                pb_update.setVisibility(View.GONE);
-//                tv_update.setVisibility(View.VISIBLE);
             }
         }).download(downloadParam);
     }
